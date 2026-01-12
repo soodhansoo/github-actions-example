e718e88b5cb417553d25a29cb99db0eab4be0b5f
soodhansoo <soodhansoo@gmail.com>
2026-01-12 15:30:08 +0530

    code

---
+++ src/test/java/com/srm/cicd/GithubCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srm.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
